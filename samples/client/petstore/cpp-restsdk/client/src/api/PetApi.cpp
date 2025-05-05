9d4459f99bc3af3d7a2fd4072c023902cccbf1f2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-05-05 04:32:10 -0700

    [cpp-rest-sdk] support exploded and deep object query parameters

--- samples/client/petstore/cpp-restsdk/client/src/api/PetApi.cpp
+++ samples/client/petstore/cpp-restsdk/client/src/api/PetApi.cpp
@@ -865,7 +865,9 @@ pplx::task<std::vector<std::shared_ptr<Pet>>> PetApi::petsPost(boost::optional<s
 
     if (page && *page != nullptr)
     {
-        localVarQueryParams[utility::conversions::to_string_t("page")] = ApiClient::parameterToString(*page);
+        for (const auto& pair : (*page)->toJson().as_object()) {
+            localVarQueryParams[utility::conversions::to_string_t(pair.first)] = ApiClient::parameterToString(pair.second.serialize());
+        }
     }
 
     std::shared_ptr<IHttpBody> localVarHttpBody;
@@ -1145,11 +1147,11 @@ pplx::task<void> PetApi::updatePetWithForm(int64_t petId, boost::optional<utilit
 
     if (name)
     {
-        localVarFormParams[ utility::conversions::to_string_t("name") ] = ApiClient::parameterToString(*name);
+        localVarFormParams[utility::conversions::to_string_t("name")] = ApiClient::parameterToString(*name);
     }
     if (status)
     {
-        localVarFormParams[ utility::conversions::to_string_t("status") ] = ApiClient::parameterToString(*status);
+        localVarFormParams[utility::conversions::to_string_t("status")] = ApiClient::parameterToString(*status);
     }
 
     std::shared_ptr<IHttpBody> localVarHttpBody;
@@ -1261,11 +1263,11 @@ pplx::task<std::shared_ptr<ApiResponse>> PetApi::uploadFile(int64_t petId, boost
 
     if (additionalMetadata)
     {
-        localVarFormParams[ utility::conversions::to_string_t("additionalMetadata") ] = ApiClient::parameterToString(*additionalMetadata);
+        localVarFormParams[utility::conversions::to_string_t("additionalMetadata")] = ApiClient::parameterToString(*additionalMetadata);
     }
     if (file && *file != nullptr)
     {
-        localVarFileParams[ utility::conversions::to_string_t("file") ] = *file;
+        localVarFileParams[utility::conversions::to_string_t("file")] = *file;
     }
 
     std::shared_ptr<IHttpBody> localVarHttpBody;
